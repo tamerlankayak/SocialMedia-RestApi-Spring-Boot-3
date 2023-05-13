e4720723877fcc6273205b845d1a691e0ba6851e
Tamerlan Mustafayev <tamerlan_94@mail.ru>
2023-05-13 23:58:44 +0400

    Update SpringSecurityConfiguration.java

--- src/main/java/com/example/springbootexamplerestfulapi/security/SpringSecurityConfiguration.java
+++ src/main/java/com/example/springbootexamplerestfulapi/security/SpringSecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+//creating basic auth
 @Configuration
 public class SpringSecurityConfiguration {
 
