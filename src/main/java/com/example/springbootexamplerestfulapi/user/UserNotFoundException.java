bb4774e145f05f15eae00eac8533592eb02b3b5e
Tamerlan Mustafayev <tamerlan_94@mail.ru>
2023-05-13 23:59:40 +0400

    Update UserNotFoundException.java

--- src/main/java/com/example/springbootexamplerestfulapi/user/UserNotFoundException.java
+++ src/main/java/com/example/springbootexamplerestfulapi/user/UserNotFoundException.java
@@ -2,7 +2,7 @@ package com.example.springbootexamplerestfulapi.user;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
-
+//exception handling
 @ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends RuntimeException {
 
